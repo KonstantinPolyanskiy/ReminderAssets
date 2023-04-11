6c0516e27db47b2baa6f84cd062a580569dc8f77
Konstantin <polyanskiy.main@list.ru>
2023-04-11 18:08:42 +0300

    test commit

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"ReminderAssets/clients/telegram_client"
 	"flag"
+	"fmt"
 	"log"
 )
 
 func main() {
 	tgBotHost := "api.telegram.org"
 	tgClient := telegram_client.NewClient(tgBotHost, mustToken())
+	fmt.Println(tgClient)
 }
 
 func mustToken() string {
